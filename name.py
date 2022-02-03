5deb714b9d1728eb90320373723dfd778a1a6beb
MTKSH <friktion0001@gmail.com>
2022-02-03 20:42:45 +0900

    gxarg today,yesterday

--- name.py
+++ name.py
@@ -1,4 +1,4 @@
-#namecube.py
+#name.py
 #python  -i
 import cv2
 import base64
@@ -12,20 +12,20 @@ import datetime
 import time
 import platform
 
-comp = pd.read_csv('tmp.txt',names=('dai','Rotation','BB','RB','difference','max','machine'))
+tmp = pd.read_csv('tmp.txt',names=('dai','Rotation','BB','RB','difference','max','machine','holl','date'))
 #dailist series to df
-posdai = comp.loc[:,'dai'].unique()
-comp.insert(0,'posdai',posdai)
+posdai = tmp.loc[:,'dai'].unique()
+tmp.insert(0,'posdai',posdai)
 dailist = pd.read_csv('dailist.txt',names=('posdai','kuu'))
-comp = pd.merge(comp, dailist, how='outer')
-comp = comp.reindex(columns=['posdai','Rotation','BB','RB','difference','max','machine'])
+merged = pd.merge(tmp, dailist, how='outer')
+reindexed = merged.reindex(columns=['posdai','Rotation','BB','RB','difference','max','machine'])
 #fillna(0) float to int64 'machine'object0 dtype to str for replacement
-comp = comp.fillna(0)
-comp = comp.astype({'posdai': 'int64','Rotation':'int64','BB':'int64','RB':'int64','difference':'int64','max':'int64','machine':'str'})
+fillnaed = reindexed.fillna(0)
+comp = fillnaed.astype({'posdai': 'int64','Rotation':'int64','BB':'int64','RB':'int64','difference':'int64','max':'int64','machine':'str'})
 #df.sort_values
 comp = comp.sort_values('posdai')
 
-
+#DailistRenameSequence
 #pd.Series.unique()
 defdai = comp.loc[:,'machine'].unique()
 
@@ -48,14 +48,9 @@ newname = (dainame.iloc[:,1]).values.tolist()
 #replace
 comp = comp.replace(machinename,newname)
 
-now = datetime.datetime.now()
-strdate = now.strftime('%m:%d %H:%M:%S')
-comp.to_csv(f'../{strdate}.csv', header=False, index=False)
+#now = datetime.datetime.now()
+#strdate = now.strftime('%m:%d %H:%M:%S')
+#comp.to_csv(f'/Users/mac2018/Applications/Collection/linkdata/{strdate}.csv', header=False, index=False)
+comp.to_csv(f'/Users/mac2018/Applications/Collection/linkdata/hakui.csv', header=False, index=False)
 
-if (comp.isnull().values.sum() != 0):
-	print ("Missing value")
-	print (comp.shape)
-else:
-	print ("OK")
-	print (comp.shape)
 quit()
